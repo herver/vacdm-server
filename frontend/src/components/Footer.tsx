2f0619da72a96dc0426361379db4a557b11de962
herver <herve@localhost.localdomain>
2025-01-08 11:55:59 +0100

    Add UTC clock in the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,19 +1,35 @@
+import dayjs from "dayjs";
 import { useEffect, useState } from "react";
 import MetaService from "services/MetaService";
 
 const Footer = () => {
   const [version, setVersion] = useState<any>([]);
+  const [clock, setClock] = useState(dayjs(new Date()).utc().format('HH:mm:ss'));
 
   useEffect(() => {
     MetaService.getVersion().then((data) => {
       setVersion(data);
     });
-  }, []);
 
+    let clockInterval = setInterval(utcTime, 1000)
+    return () => {
+      clearInterval(clockInterval);
+    }
+  }, []);
+  
+  function utcTime() {
+    return setClock(dayjs(new Date()).utc().format('HH:mm:ss'));
+  }
+  
   return (
     <>
       <div className="footer">
-        VATSIM Airport Collaboration Decision Making Version {version.version}
+        <ul>
+          <li>VATSIM Airport Collaboration Decision Making v{version.version}</li>
+        </ul>
+        <ul>
+          <li>{clock} UTC</li>
+        </ul>
       </div>
     </>
   );
